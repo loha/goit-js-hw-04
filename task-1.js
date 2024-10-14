74e8d483bfef7e5d15dffce740ff80d9f3647695
Oleksii Nosov <o.nosov@turbogames.io>
2024-10-14 15:01:10 +0300

    MAIN

---
+++ task-1.js
@@ -0,0 +1,13 @@
+function isEnoughCapacity(availableProducts, order) {
+  const totalProducts = Object.values(availableProducts).reduce(
+    (acc, value) => acc + value,
+    0
+  );
+
+  return totalProducts <= order;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
